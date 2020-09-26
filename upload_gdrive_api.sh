a7340467182c20eb44ce82cb19f74a7dfe2cca8f
Florents Tselai <florents.tselai@gmail.com>
2020-09-26 23:31:21 +0300

    Always use a refreshed token

--- upload_gdrive_api.sh
+++ upload_gdrive_api.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Get a new token using a refreshed one
+access_token=`curl -s --request POST --data "client_id=$GDRIVE_API_CLIENT_ID&client_secret=$GDRIVE_API_CLIENT_SECRET&refresh_token=$GDRIVE_API_REFRESH_TOKEN&grant_type=refresh_token" https://accounts.google.com/o/oauth2/token | jq .access_token`
+
 file=$1
 mime_type=`file -b --mime-type $1`
 
 echo $file $mime_type
 
 curl -X POST -L \
-    -H "Authorization: Bearer `echo $GDRIVE_API_TOKEN`" \
+    -H "Authorization: Bearer `echo $access_token`" \
     -F "metadata={name : '$file'};type=application/json;charset=UTF-8" \
     -F "file=@$file;type=$mime_type" \
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
