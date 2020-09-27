8c3bee9d7d0f1d09305e87c3dadf7dd673db9866
Florents Tselai <florents.tselai@gmail.com>
2020-09-27 20:41:01 +0300

    Formatting

--- add-to-pocket.sh
+++ add-to-pocket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -sL \
--H "Content-Type: application/json; charset=UTF-8" \
--H "X-Accept: application/json" \
--d "{\"url\": \"$1\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
--X POST "https://getpocket.com/v3/add" | jq '.item.title'
+  -H "Content-Type: application/json; charset=UTF-8" \
+  -H "X-Accept: application/json" \
+  -d "{\"url\": \"$1\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
+  -X POST "https://getpocket.com/v3/add" | jq '.item.title'
--- daily_newspapers_in_your_pocket.sh
+++ daily_newspapers_in_your_pocket.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
 for url in \
-https://www.nytimes.com/section/todayspaper \
-https://www.washingtonpost.com/todays_paper/updates \
-https://economist.com https://www.wsj.com/news/types/bookshelf \
-https://www.nytimes.com/section/books \
-https://www.economist.com/books-and-arts/ \
-https://newyorker.com https://theatlantic.com \
-https://www.prospectmagazine.co.uk/category/arts-and-books \
-https://www.washingtonpost/entertainment/books \
-https://news.ycombinator.com/best; do
-	curl -L -H "Content-Type: application/json; charset=UTF-8" -H "X-Accept: application/json" \
--d "{\"url\": \"$url\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
--X POST "https://getpocket.com/v3/add"
+  https://www.nytimes.com/section/todayspaper \
+  https://www.washingtonpost.com/todays_paper/updates \
+  https://economist.com https://www.wsj.com/news/types/bookshelf \
+  https://www.nytimes.com/section/books \
+  https://www.economist.com/books-and-arts/ \
+  https://newyorker.com https://theatlantic.com \
+  https://www.prospectmagazine.co.uk/category/arts-and-books \
+  https://www.washingtonpost/entertainment/books \
+  https://news.ycombinator.com/best; do
+  curl -L \
+    -H "Content-Type: application/json; charset=UTF-8" \
+    -H "X-Accept: application/json" \
+    -d "{\"url\": \"$url\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
+    -X POST "https://getpocket.com/v3/add"
 done
 
-
 # WSJ requires special care
-for section in frontpage us world opinion business review offduty lifearts new-york; do
-	url=https://www.wsj.com/print-edition/$(date +'%Y%m%d')/$section
-	curl -L -H "Content-Type: application/json; charset=UTF-8" -H "X-Accept: application/json" \
--d "{\"url\": \"$url\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
--X POST "https://getpocket.com/v3/add"
+for section in \
+  frontpage us world opinion business review offduty lifearts new-york; do
+  url=https://www.wsj.com/print-edition/$(date +'%Y%m%d')/$section
+  curl -L \
+    -H "Content-Type: application/json; charset=UTF-8" \
+    -H "X-Accept: application/json" \
+    -d "{\"url\": \"$url\", \"consumer_key\": \"$POCKET_CONSUMER_KEY\",\"access_token\": \"$POCKET_ACCESS_TOKEN\"}" \
+    -X POST "https://getpocket.com/v3/add"
 done
